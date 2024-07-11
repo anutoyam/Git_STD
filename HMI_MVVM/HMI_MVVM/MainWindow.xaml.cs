65b6a775b55c4fd86d4244b6f17cee360495155d
anutoyam <akflshdev@gmail.com>
2024-07-11 15:51:06 +0900

    ~4장

--- HMI_MVVM/HMI_MVVM/MainWindow.xaml.cs
+++ HMI_MVVM/HMI_MVVM/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HMI_MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,29 @@ namespace HMI_MVVM
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+        private void ButtonTest1_Click(object sender, RoutedEventArgs e)
+        {
+            List<User> myList1 = new List<User>();
+            labelTest1.Content = "내용변경완료";
+            checkBoxTest1.IsChecked = !checkBoxTest1.IsChecked;
+            //MessageBox.Show(textBoxTest1.Text);
+            User userA = new User();
+            userA.Name = "Tuna";
+            userA.UserAge = 33;
+            userA.UserImg = @"C:\Users\MH\Source\Repos\anutoyam\Git_STD\HMI_MVVM\HMI_MVVM\Resources\photo1.jpg";
+
+            User userB = new User();
+            userB.Name = "Mayo";
+            userB.UserAge = 33;
+            userB.UserImg = @"C:\Users\MH\Source\Repos\anutoyam\Git_STD\HMI_MVVM\HMI_MVVM\Resources\photo1.jpg";
+
+            myList1.Add(userA);
+            myList1.Add(userB);
+
+            listView1.ItemsSource = myList1;
+        }
+
     }
 }
